c303cf2207b7af2857c2b9afec29de402eddafde
Luis Humberto <beto020017@gmail.com>
2021-06-05 16:54:23 -0500

    Cambio en el diseño de la vista principal

--- Intento de forfmulario/ContentView.swift
+++ Intento de forfmulario/ContentView.swift
@@ -13,29 +13,58 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             VStack(alignment: .center, spacing: 40){
-                    
-                NavigationLink(destination: Harris(),label: {Text("Harris-Benedict")})
-                    .navigationBarTitle("Formulas", displayMode: .inline)
-                    .padding(.top, 50)
-                    
-                NavigationLink(destination: VistaOMS(),label: {Text("FAM/OMS")})
-
-                NavigationLink(destination: vistaIOM(),label: {Text("IOM")})
-                    
-                NavigationLink(destination: VistaOMS(),label: {Text("Mifflin")})
                 Spacer()
-            }
+                NavigationLink(destination: Harris(),label: {Text("Harris-Benedict") .padding(.all,25)})
+                    .navigationBarTitle("Formulas", displayMode: .inline)
+                    .background(Color.orange)
+                    .cornerRadius(16)
+                    .foregroundColor(.white)
+                    
+                NavigationLink(destination: VistaOMS(),label: {Text("FAM/OMS").padding(.all,25)})
+                    .navigationBarTitle("Formulas", displayMode: .inline)
+                    .background(Color.orange)
+                    .cornerRadius(16)
+                    .foregroundColor(.white)
+
+                NavigationLink(destination: vistaIOM(),label: {Text("IOM").padding(.all,25)})
+                    .navigationBarTitle("Formulas", displayMode: .inline)
+                    .background(Color.orange)
+                    .cornerRadius(16)
+                    .foregroundColor(.white)
+                    
+                NavigationLink(destination: VistaOMS(),label: {Text("Mifflin").padding(.all,25)})
+                    .navigationBarTitle("Formulas", displayMode: .inline)
+                    .background(Color.orange)
+                    .cornerRadius(16)
+                    .foregroundColor(.white)
+                Spacer()
+            }.frame(width: 500)
+            .background(LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .bottom, endPoint: .top))
+        
         }
         
     }
 }
 
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .background(Color.green)
     }
 }
 
+
+/*NavigationView{
+    VStack(alignment: .center, spacing: 40){
+            
+        NavigationLink(destination: Harris(),label: {Text("Harris-Benedict")})
+            .navigationBarTitle("Formulas", displayMode: .inline)
+            .padding(.top, 50)
+            
+        NavigationLink(destination: VistaOMS(),label: {Text("FAM/OMS")})
+
+        NavigationLink(destination: vistaIOM(),label: {Text("IOM")})
+            
+        NavigationLink(destination: VistaOMS(),label: {Text("Mifflin")})
+        Spacer()
+    }
+}*/
