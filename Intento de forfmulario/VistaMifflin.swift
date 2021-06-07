969a3c222cc16abba82060d12d9219beb2125d9b
Luis Humberto <beto020017@gmail.com>
2021-06-06 22:39:46 -0500

    Vista mifflin

---
+++ Intento de forfmulario/VistaMifflin.swift
@@ -0,0 +1,20 @@
+//
+//  VistaMifflin.swift
+//  Intento de forfmulario
+//
+//  Created by Luis Humberto Martinez Echegaray on 06/06/21.
+//
+
+import SwiftUI
+
+struct VistaMifflin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VistaMifflin_Previews: PreviewProvider {
+    static var previews: some View {
+        VistaMifflin()
+    }
+}
